0946dc9fb655b938244b147530963a26894cfc1b
Linus Gasser <linus.gasser@epfl.ch>
2021-05-31 08:03:53 +0200

    Initial Commit

---
+++ arti-ios-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  arti-ios-test
+//
+//  Created by Linus Gasser on 31.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ arti-ios-test/arti_ios_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  arti_ios_testApp.swift
+//  arti-ios-test
+//
+//  Created by Linus Gasser on 31.05.21.
+//
+
+import SwiftUI
+
+@main
+struct arti_ios_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
